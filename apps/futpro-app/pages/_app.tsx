3133a13da0dfb1034baf0dd1e5db043cc82e2f37
wildpirus <wildpirus@gmail.com>
2021-11-24 19:45:32 -0500

    fc

--- apps/futpro-app/pages/_app.tsx
+++ apps/futpro-app/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import {Button as Bttn} from '@futpro/ui';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -17,6 +18,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <main>
           <Component {...pageProps} />
         </main>
+        <Bttn>Nojoda cule filo</Bttn> 
       </div>
     </>
   );
---
+++ libs/ui/src/lib/button/button.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+/* eslint-disable-next-line */
+export interface ButtonProps {}
+
+const StyledButton = styled.div`
+  color: pink;
+`;
+
+export function Button(props: ButtonProps) {
+  return (
+    <StyledButton>
+      <h1>Welcome to Button!</h1>
+    </StyledButton>
+  );
+}
+
+export default Button;
+
